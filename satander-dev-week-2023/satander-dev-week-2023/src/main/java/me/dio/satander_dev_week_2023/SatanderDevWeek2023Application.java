bb334b5a9f7c123f42d78427465f59f9813e4270
Gabriel Nogueira <gabrielnogueiras@outlook.com>
2025-04-24 22:47:39 -0300

    Add files via upload

---
+++ satander-dev-week-2023/satander-dev-week-2023/src/main/java/me/dio/satander_dev_week_2023/SatanderDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.satander_dev_week_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatanderDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatanderDevWeek2023Application.class, args);
+	}
+
+}
---
+++ satander-dev-week-2023/satander-dev-week-2023/src/test/java/me/dio/satander_dev_week_2023/SatanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.satander_dev_week_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
